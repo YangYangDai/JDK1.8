4730a1be645dee605dfb59d0d3198086adcf6183
yangyangdai <1071617783@qq.com>
2019-07-28 20:59:05 +0800

    添加Stack源码解析

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ## JDK1.8源码分析
 ### 容器类分析
 1. [Vector](https://github.com/YangYangDai/JDK1.8/blob/master/src/java/util/Vector.java "Vector")
-2. [ArrayList](https://github.com/YangYangDai/JDK1.8/blob/master/src/java/util/ArrayList.java "ArrayList")
-3. [LinkedList](https://github.com/YangYangDai/JDK1.8/blob/master/src/java/util/LinkedList.java "LinkedList")
-4. [HashMap](https://github.com/YangYangDai/JDK1.8/blob/master/src/java/util/HashMap.java "HashMap")
-5. [HashSet](https://github.com/YangYangDai/JDK1.8/blob/master/src/java/util/HashSet.java "HashSet")
-6. [LinkedHashMap](https://github.com/YangYangDai/JDK1.8/blob/master/src/java/util/LinkedHashMap.java "LinkedHashMap")
+2. [Stack](https://github.com/YangYangDai/JDK1.8/blob/master/src/java/util/Stack.java "Stack")
+3. [ArrayList](https://github.com/YangYangDai/JDK1.8/blob/master/src/java/util/ArrayList.java "ArrayList")
+4. [LinkedList](https://github.com/YangYangDai/JDK1.8/blob/master/src/java/util/LinkedList.java "LinkedList")
+5. [HashMap](https://github.com/YangYangDai/JDK1.8/blob/master/src/java/util/HashMap.java "HashMap")
+6. [HashSet](https://github.com/YangYangDai/JDK1.8/blob/master/src/java/util/HashSet.java "HashSet")
+7. [LinkedHashMap](https://github.com/YangYangDai/JDK1.8/blob/master/src/java/util/LinkedHashMap.java "LinkedHashMap")
 ------------
 ### java并发工具类 java.util.concurent(J.U.C)
 1. [Semaphore](https://github.com/YangYangDai/JDK1.8/blob/master/src/java/util/concurrent/Semaphore.java "Semaphore")
--- src/java/util/Stack.java
+++ src/java/util/Stack.java
@@ -26,42 +26,20 @@
 package java.util;
 
 /**
- * The <code>Stack</code> class represents a last-in-first-out
- * (LIFO) stack of objects. It extends class <tt>Vector</tt> with five
- * operations that allow a vector to be treated as a stack. The usual
- * <tt>push</tt> and <tt>pop</tt> operations are provided, as well as a
- * method to <tt>peek</tt> at the top item on the stack, a method to test
- * for whether the stack is <tt>empty</tt>, and a method to <tt>search</tt>
- * the stack for an item and discover how far it is from the top.
- * <p>
- * When a stack is first created, it contains no items.
- *
- * <p>A more complete and consistent set of LIFO stack operations is
- * provided by the {@link Deque} interface and its implementations, which
- * should be used in preference to this class.  For example:
- * <pre>   {@code
- *   Deque<Integer> stack = new ArrayDeque<Integer>();}</pre>
- *
- * @author  Jonathan Payne
- * @since   JDK1.0
+ * 栈
+ * 先进后出
+ * 继承自Vector 线程安全的但为此开销也会大
+ * 底层也是由数组实现
+ * 不推荐使用
  */
 public
 class Stack<E> extends Vector<E> {
-    /**
-     * Creates an empty Stack.
-     */
+    
     public Stack() {
     }
 
     /**
-     * Pushes an item onto the top of this stack. This has exactly
-     * the same effect as:
-     * <blockquote><pre>
-     * addElement(item)</pre></blockquote>
-     *
-     * @param   item   the item to be pushed onto this stack.
-     * @return  the <code>item</code> argument.
-     * @see     java.util.Vector#addElement
+     * 入栈 栈顶入栈
      */
     public E push(E item) {
         addElement(item);
@@ -70,30 +48,20 @@ class Stack<E> extends Vector<E> {
     }
 
     /**
-     * Removes the object at the top of this stack and returns that
-     * object as the value of this function.
-     *
-     * @return  The object at the top of this stack (the last item
-     *          of the <tt>Vector</tt> object).
-     * @throws  EmptyStackException  if this stack is empty.
+     * 出栈  栈顶出栈
      */
     public synchronized E pop() {
         E       obj;
         int     len = size();
-
+        //获取数组
         obj = peek();
+        //删除尾部的数据
         removeElementAt(len - 1);
-
         return obj;
     }
 
     /**
-     * Looks at the object at the top of this stack without removing it
-     * from the stack.
-     *
-     * @return  the object at the top of this stack (the last item
-     *          of the <tt>Vector</tt> object).
-     * @throws  EmptyStackException  if this stack is empty.
+     *	获取栈顶数据
      */
     public synchronized E peek() {
         int     len = size();
@@ -104,28 +72,14 @@ class Stack<E> extends Vector<E> {
     }
 
     /**
-     * Tests if this stack is empty.
-     *
-     * @return  <code>true</code> if and only if this stack contains
-     *          no items; <code>false</code> otherwise.
+     * 判断栈是否为空
      */
     public boolean empty() {
         return size() == 0;
     }
 
     /**
-     * Returns the 1-based position where an object is on this stack.
-     * If the object <tt>o</tt> occurs as an item in this stack, this
-     * method returns the distance from the top of the stack of the
-     * occurrence nearest the top of the stack; the topmost item on the
-     * stack is considered to be at distance <tt>1</tt>. The <tt>equals</tt>
-     * method is used to compare <tt>o</tt> to the
-     * items in this stack.
-     *
-     * @param   o   the desired object.
-     * @return  the 1-based position from the top of the stack where
-     *          the object is located; the return value <code>-1</code>
-     *          indicates that the object is not on the stack.
+     * 在栈中查找o的下标
      */
     public synchronized int search(Object o) {
         int i = lastIndexOf(o);
