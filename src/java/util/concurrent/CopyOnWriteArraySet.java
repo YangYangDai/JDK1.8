ab85207b1be0b0cc1450e9238842cf186a25a385
yangyangdai <1071617783@qq.com>
2019-08-27 22:27:02 +0800

    添加CopyOnWriteArraySet的源码解析

--- src/java/util/concurrent/CopyOnWriteArraySet.java
+++ src/java/util/concurrent/CopyOnWriteArraySet.java
@@ -1,38 +1,3 @@
-/*
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
-/*
- *
- *
- *
- *
- *
- * Written by Doug Lea with assistance from members of JCP JSR-166
- * Expert Group and released to the public domain, as explained at
- * http://creativecommons.org/publicdomain/zero/1.0/
- */
-
 package java.util.concurrent;
 import java.util.Collection;
 import java.util.Set;
@@ -44,343 +9,194 @@ import java.util.function.Predicate;
 import java.util.function.Consumer;
 
 /**
- * A {@link java.util.Set} that uses an internal {@link CopyOnWriteArrayList}
- * for all of its operations.  Thus, it shares the same basic properties:
- * <ul>
- *  <li>It is best suited for applications in which set sizes generally
- *       stay small, read-only operations
- *       vastly outnumber mutative operations, and you need
- *       to prevent interference among threads during traversal.
- *  <li>It is thread-safe.
- *  <li>Mutative operations ({@code add}, {@code set}, {@code remove}, etc.)
- *      are expensive since they usually entail copying the entire underlying
- *      array.
- *  <li>Iterators do not support the mutative {@code remove} operation.
- *  <li>Traversal via iterators is fast and cannot encounter
- *      interference from other threads. Iterators rely on
- *      unchanging snapshots of the array at the time the iterators were
- *      constructed.
- * </ul>
- *
- * <p><b>Sample Usage.</b> The following code sketch uses a
- * copy-on-write set to maintain a set of Handler objects that
- * perform some action upon state updates.
- *
- *  <pre> {@code
- * class Handler { void handle(); ... }
- *
- * class X {
- *   private final CopyOnWriteArraySet<Handler> handlers
- *     = new CopyOnWriteArraySet<Handler>();
- *   public void addHandler(Handler h) { handlers.add(h); }
- *
- *   private long internalState;
- *   private synchronized void changeState() { internalState = ...; }
- *
- *   public void update() {
- *     changeState();
- *     for (Handler handler : handlers)
- *       handler.handle();
- *   }
- * }}</pre>
- *
- * <p>This class is a member of the
- * <a href="{@docRoot}/../technotes/guides/collections/index.html">
- * Java Collections Framework</a>.
- *
- * @see CopyOnWriteArrayList
- * @since 1.5
- * @author Doug Lea
- * @param <E> the type of elements held in this collection
+ * 	CopyOnWriteArraySet
+ * 	Set:数据项不允许重复的
+ *	具体实现由内部属性CopyOnWriteArrayList完成
+ *	array在读操作的时候会被改变 读操作操作的是旧array
+ * 	写操作直接用锁锁住对应的操作，不会去直接改变之前的array，而是操作新的数组，所以读操作都是不用锁的，因为读的是旧数据
+ * 	适用于读多写少的场景
  */
 public class CopyOnWriteArraySet<E> extends AbstractSet<E>
         implements java.io.Serializable {
     private static final long serialVersionUID = 5457747651344034263L;
-
+    /**
+     *	具体的实现都是由CopyOnWriteArrayList完成
+     */
     private final CopyOnWriteArrayList<E> al;
 
     /**
-     * Creates an empty set.
+     * 	初始属性CopyOnWriteArrayList
      */
     public CopyOnWriteArraySet() {
         al = new CopyOnWriteArrayList<E>();
     }
 
     /**
-     * Creates a set containing all of the elements of the specified
-     * collection.
-     *
-     * @param c the collection of elements to initially contain
-     * @throws NullPointerException if the specified collection is null
+     * 	将集合中的数据放入array中
+     * @param c 集合
      */
     public CopyOnWriteArraySet(Collection<? extends E> c) {
+    	//集合属于CopyOnWriteArraySet
         if (c.getClass() == CopyOnWriteArraySet.class) {
+        	//先强转
             @SuppressWarnings("unchecked") CopyOnWriteArraySet<E> cc =
                 (CopyOnWriteArraySet<E>)c;
+            //初始属性CopyOnWriteArrayList 传入CopyOnWriteArraySet中的al
             al = new CopyOnWriteArrayList<E>(cc.al);
         }
         else {
+        	//初始属性CopyOnWriteArrayList
             al = new CopyOnWriteArrayList<E>();
+            //添加没有存在于数组中的集合数据项
             al.addAllAbsent(c);
         }
     }
 
     /**
-     * Returns the number of elements in this set.
+     * 	返回set的大小
      *
      * @return the number of elements in this set
      */
     public int size() {
+    	//调用CopyOnWriteArrayList中的方法
         return al.size();
     }
 
     /**
-     * Returns {@code true} if this set contains no elements.
-     *
-     * @return {@code true} if this set contains no elements
+     *	判断集合是否为null
+     *@param true set的为空 false 不为空
      */
     public boolean isEmpty() {
+    	//调用CopyOnWriteArrayList中的方法
         return al.isEmpty();
     }
 
     /**
-     * Returns {@code true} if this set contains the specified element.
-     * More formally, returns {@code true} if and only if this set
-     * contains an element {@code e} such that
-     * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>.
-     *
-     * @param o element whose presence in this set is to be tested
-     * @return {@code true} if this set contains the specified element
+     * 	判断对象是否存在set中
+     * @param o 对象
+     * @return true 存在  false 不存在
      */
     public boolean contains(Object o) {
+    	//调用CopyOnWriteArrayList中的方法
         return al.contains(o);
     }
 
     /**
-     * Returns an array containing all of the elements in this set.
-     * If this set makes any guarantees as to what order its elements
-     * are returned by its iterator, this method must return the
-     * elements in the same order.
-     *
-     * <p>The returned array will be "safe" in that no references to it
-     * are maintained by this set.  (In other words, this method must
-     * allocate a new array even if this set is backed by an array).
-     * The caller is thus free to modify the returned array.
-     *
-     * <p>This method acts as bridge between array-based and collection-based
-     * APIs.
-     *
-     * @return an array containing all the elements in this set
+     *	获取对象数组
      */
     public Object[] toArray() {
+    	//调用CopyOnWriteArrayList中的方法
         return al.toArray();
     }
 
     /**
-     * Returns an array containing all of the elements in this set; the
-     * runtime type of the returned array is that of the specified array.
-     * If the set fits in the specified array, it is returned therein.
-     * Otherwise, a new array is allocated with the runtime type of the
-     * specified array and the size of this set.
-     *
-     * <p>If this set fits in the specified array with room to spare
-     * (i.e., the array has more elements than this set), the element in
-     * the array immediately following the end of the set is set to
-     * {@code null}.  (This is useful in determining the length of this
-     * set <i>only</i> if the caller knows that this set does not contain
-     * any null elements.)
-     *
-     * <p>If this set makes any guarantees as to what order its elements
-     * are returned by its iterator, this method must return the elements
-     * in the same order.
-     *
-     * <p>Like the {@link #toArray()} method, this method acts as bridge between
-     * array-based and collection-based APIs.  Further, this method allows
-     * precise control over the runtime type of the output array, and may,
-     * under certain circumstances, be used to save allocation costs.
-     *
-     * <p>Suppose {@code x} is a set known to contain only strings.
-     * The following code can be used to dump the set into a newly allocated
-     * array of {@code String}:
-     *
-     *  <pre> {@code String[] y = x.toArray(new String[0]);}</pre>
-     *
-     * Note that {@code toArray(new Object[0])} is identical in function to
-     * {@code toArray()}.
-     *
-     * @param a the array into which the elements of this set are to be
-     *        stored, if it is big enough; otherwise, a new array of the same
-     *        runtime type is allocated for this purpose.
-     * @return an array containing all the elements in this set
-     * @throws ArrayStoreException if the runtime type of the specified array
-     *         is not a supertype of the runtime type of every element in this
-     *         set
-     * @throws NullPointerException if the specified array is null
+     *	获取T类型的数组
+     * @param a 类型数组
      */
     public <T> T[] toArray(T[] a) {
+    	//调用CopyOnWriteArrayList中的方法
         return al.toArray(a);
     }
 
     /**
-     * Removes all of the elements from this set.
-     * The set will be empty after this call returns.
+     * 	清空所有数据项
      */
     public void clear() {
+    	//调用CopyOnWriteArrayList中的方法
         al.clear();
     }
 
     /**
-     * Removes the specified element from this set if it is present.
-     * More formally, removes an element {@code e} such that
-     * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>,
-     * if this set contains such an element.  Returns {@code true} if
-     * this set contained the element (or equivalently, if this set
-     * changed as a result of the call).  (This set will not contain the
-     * element once the call returns.)
-     *
-     * @param o object to be removed from this set, if present
-     * @return {@code true} if this set contained the specified element
+     *	移除数据项o
+     * @param o 移除对象
+     * @return true 移除成功 false 未发生移除
      */
     public boolean remove(Object o) {
+    	//调用CopyOnWriteArrayList中的方法
         return al.remove(o);
     }
 
     /**
-     * Adds the specified element to this set if it is not already present.
-     * More formally, adds the specified element {@code e} to this set if
-     * the set contains no element {@code e2} such that
-     * <tt>(e==null&nbsp;?&nbsp;e2==null&nbsp;:&nbsp;e.equals(e2))</tt>.
-     * If this set already contains the element, the call leaves the set
-     * unchanged and returns {@code false}.
-     *
-     * @param e element to be added to this set
-     * @return {@code true} if this set did not already contain the specified
-     *         element
+     *	添加数据  如果数据不存在才添加 保证数据项的唯一
+     * @param e 数据项
+     * @return true 不存在且添加成功 false 数据项已存在
      */
     public boolean add(E e) {
+    	//调用CopyOnWriteArrayList中的方法
         return al.addIfAbsent(e);
     }
 
     /**
-     * Returns {@code true} if this set contains all of the elements of the
-     * specified collection.  If the specified collection is also a set, this
-     * method returns {@code true} if it is a <i>subset</i> of this set.
-     *
-     * @param  c collection to be checked for containment in this set
-     * @return {@code true} if this set contains all of the elements of the
-     *         specified collection
-     * @throws NullPointerException if the specified collection is null
-     * @see #contains(Object)
+     *	判断是否包含集合中的所有数据项
+     * @param  c 集合
+     * @return true 包含所有  false 至少有一个不包含
      */
     public boolean containsAll(Collection<?> c) {
+    	//调用CopyOnWriteArrayList中的方法
         return al.containsAll(c);
     }
 
     /**
-     * Adds all of the elements in the specified collection to this set if
-     * they're not already present.  If the specified collection is also a
-     * set, the {@code addAll} operation effectively modifies this set so
-     * that its value is the <i>union</i> of the two sets.  The behavior of
-     * this operation is undefined if the specified collection is modified
-     * while the operation is in progress.
-     *
-     * @param  c collection containing elements to be added to this set
-     * @return {@code true} if this set changed as a result of the call
-     * @throws NullPointerException if the specified collection is null
-     * @see #add(Object)
+     *	添加集合中的数据项  数据项之前不存在与set中
+     * @param  c 集合
+     * @return true 至少添加了一个数据项 false 没有添加数据项
      */
     public boolean addAll(Collection<? extends E> c) {
+    	//调用CopyOnWriteArrayList中的方法
         return al.addAllAbsent(c) > 0;
     }
 
     /**
-     * Removes from this set all of its elements that are contained in the
-     * specified collection.  If the specified collection is also a set,
-     * this operation effectively modifies this set so that its value is the
-     * <i>asymmetric set difference</i> of the two sets.
-     *
-     * @param  c collection containing elements to be removed from this set
-     * @return {@code true} if this set changed as a result of the call
-     * @throws ClassCastException if the class of an element of this set
-     *         is incompatible with the specified collection (optional)
-     * @throws NullPointerException if this set contains a null element and the
-     *         specified collection does not permit null elements (optional),
-     *         or if the specified collection is null
+     *	移除集合c中存在于set中的数据项
+     * @param  c 集合
+     * @return true 至少移除了一个数据项 false 没有移除数据项
      * @see #remove(Object)
      */
     public boolean removeAll(Collection<?> c) {
+    	//调用CopyOnWriteArrayList中的方法
         return al.removeAll(c);
     }
 
     /**
-     * Retains only the elements in this set that are contained in the
-     * specified collection.  In other words, removes from this set all of
-     * its elements that are not contained in the specified collection.  If
-     * the specified collection is also a set, this operation effectively
-     * modifies this set so that its value is the <i>intersection</i> of the
-     * two sets.
-     *
-     * @param  c collection containing elements to be retained in this set
-     * @return {@code true} if this set changed as a result of the call
-     * @throws ClassCastException if the class of an element of this set
-     *         is incompatible with the specified collection (optional)
-     * @throws NullPointerException if this set contains a null element and the
-     *         specified collection does not permit null elements (optional),
-     *         or if the specified collection is null
+     *	移除集合c中不存在于set中的数据项
+     * @param  c 集合
+     * @return true 至少移除了一个数据项 false 没有移除数据项
      * @see #remove(Object)
      */
     public boolean retainAll(Collection<?> c) {
+    	//调用CopyOnWriteArrayList中的方法
         return al.retainAll(c);
     }
 
     /**
-     * Returns an iterator over the elements contained in this set
-     * in the order in which these elements were added.
-     *
-     * <p>The returned iterator provides a snapshot of the state of the set
-     * when the iterator was constructed. No synchronization is needed while
-     * traversing the iterator. The iterator does <em>NOT</em> support the
-     * {@code remove} method.
-     *
-     * @return an iterator over the elements in this set
+     *	获取Iterator
      */
     public Iterator<E> iterator() {
+    	//调用CopyOnWriteArrayList中的方法
         return al.iterator();
     }
 
     /**
-     * Compares the specified object with this set for equality.
-     * Returns {@code true} if the specified object is the same object
-     * as this object, or if it is also a {@link Set} and the elements
-     * returned by an {@linkplain Set#iterator() iterator} over the
-     * specified set are the same as the elements returned by an
-     * iterator over this set.  More formally, the two iterators are
-     * considered to return the same elements if they return the same
-     * number of elements and for every element {@code e1} returned by
-     * the iterator over the specified set, there is an element
-     * {@code e2} returned by the iterator over this set such that
-     * {@code (e1==null ? e2==null : e1.equals(e2))}.
-     *
-     * @param o object to be compared for equality with this set
+     *	判断是否相等
+     * @param o 
      * @return {@code true} if the specified object is equal to this set
      */
     public boolean equals(Object o) {
+    	//内存地址相等 直接返回true
         if (o == this)
             return true;
+        //o不属于set直接返回
         if (!(o instanceof Set))
             return false;
         Set<?> set = (Set<?>)(o);
         Iterator<?> it = set.iterator();
-
-        // Uses O(n^2) algorithm that is only appropriate
-        // for small sets, which CopyOnWriteArraySets should be.
-
-        //  Use a single snapshot of underlying array
+        
         Object[] elements = al.getArray();
         int len = elements.length;
         // Mark matched elements to avoid re-checking
         boolean[] matched = new boolean[len];
         int k = 0;
+        //判断两个数组是数据项是否完全相等
         outer: while (it.hasNext()) {
             if (++k > len)
                 return false;
@@ -405,19 +221,7 @@ public class CopyOnWriteArraySet<E> extends AbstractSet<E>
     }
 
     /**
-     * Returns a {@link Spliterator} over the elements in this set in the order
-     * in which these elements were added.
-     *
-     * <p>The {@code Spliterator} reports {@link Spliterator#IMMUTABLE},
-     * {@link Spliterator#DISTINCT}, {@link Spliterator#SIZED}, and
-     * {@link Spliterator#SUBSIZED}.
-     *
-     * <p>The spliterator provides a snapshot of the state of the set
-     * when the spliterator was constructed. No synchronization is needed while
-     * operating on the spliterator.
-     *
-     * @return a {@code Spliterator} over the elements in this set
-     * @since 1.8
+     *	spliterator
      */
     public Spliterator<E> spliterator() {
         return Spliterators.spliterator
@@ -425,7 +229,7 @@ public class CopyOnWriteArraySet<E> extends AbstractSet<E>
     }
 
     /**
-     * Tests for equality, coping with nulls.
+     * 	判断对象是否相等
      */
     private static boolean eq(Object o1, Object o2) {
         return (o1 == null) ? o2 == null : o1.equals(o2);
